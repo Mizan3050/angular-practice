273a5ff0190a38e91b195fc65bab1dbc62a6fe94
Meezan Shaikh <meezan@tradexa.co.in>
2024-12-20 22:55:53 +0530

    attaching actions with data

--- src/app/feature/ngrx-demo/counter-controller/counter-controller.component.ts
+++ src/app/feature/ngrx-demo/counter-controller/counter-controller.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { increment, decrement } from '../store/counter.actions';
 
 @Component({
   selector: 'app-counter-controller',
@@ -8,5 +10,15 @@ import { Component } from '@angular/core';
   styleUrl: './counter-controller.component.scss'
 })
 export class CounterControllerComponent {
+  constructor(private store: Store) {
 
+  }
+
+  increment() {
+    this.store.dispatch(increment({value: 2}))
+  }
+
+  decrement() {
+    this.store.dispatch(decrement({value: 2}))
+  }
 }
