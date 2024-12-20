98e12b7d6ee7feac38904f8dbb350db114c65e5d
Meezan Shaikh <meezan@tradexa.co.in>
2024-12-21 00:24:45 +0530

    effects implemented

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,9 +6,10 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideStore } from '@ngrx/store';
 import { counterReducer } from './feature/ngrx-demo/store/counter.reducer';
 import { provideEffects } from '@ngrx/effects';
+import { CounterEffects } from './feature/ngrx-demo/store/counter.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideStore({
-        counter: counterReducer
-    }), provideEffects()]
+    counter: counterReducer
+  }), provideEffects([CounterEffects])]
 };
---
+++ src/app/feature/ngrx-demo/store/counter.effects.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { decrement, increment } from "./counter.actions";
+import { tap, withLatestFrom } from "rxjs";
+import { Store } from "@ngrx/store";
+import { selectCount } from "./counter.selector";
+
+
+@Injectable()
+export class CounterEffects {
+    saveCount = createEffect(() =>
+        this.actions$.pipe(
+            ofType(increment, decrement),
+            withLatestFrom(this.store.select(selectCount)),
+            tap(([action, counter]) =>
+                localStorage.setItem('count', counter.toString()))
+        ), {
+        dispatch: false
+    })
+
+    constructor(private actions$: Actions, private store: Store<{ counter: number }>) {
+    }
+}
